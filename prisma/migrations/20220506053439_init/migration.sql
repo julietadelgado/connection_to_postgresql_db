783d6936745de72d3e8892c94c0deb39be3a13b4
Julieta Delgado <julidmeza@gmail.com>
2022-05-06 00:35:28 -0500

    Add model Student and create migration

---
+++ prisma/migrations/20220506053439_init/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "Student" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "lang" VARCHAR(255) NOT NULL,
+    "missionCommander" VARCHAR(255) NOT NULL,
+    "enrollments" INTEGER NOT NULL,
+    "hasCertification" BOOLEAN NOT NULL DEFAULT false,
+    "dateCreated" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "lastUpdated" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Student_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Student_name_key" ON "Student"("name");
